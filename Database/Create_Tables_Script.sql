0c9959dee9aefc78d35942dde9c770bf3dcf42a2
NORDBY\Mathias <Mathias_nordby@hotmail.com>
2017-05-15 13:51:56 +0200

    rename: Php-folder + UPDATE need to be complete

--- Database/Create_Tables_Script.sql
+++ Database/Create_Tables_Script.sql
@@ -1,15 +1,17 @@
 DROP TABLE IF EXISTS accounts;
 DROP TABLE IF EXISTS deleted_accounts;
+DROP TABLE IF EXISTS login_attempts;
+
 CREATE TABLE accounts (
 id INT(11) UNIQUE NOT NULL AUTO_INCREMENT,
 username VARCHAR(255) NOT NULL UNIQUE,
-pass VARCHAR(16) NOT NULL,
+pass CHAR(128) NOT NULL, /* CHAR(128) = So it adds up with SHA512 algoritm */
 email VARCHAR(255) NOT NULL,
 balance INT(255) DEFAULT 0,
 location VARCHAR(255),
 lt_online VARCHAR(255),
-account_status bit(1) default 0,
-is_deleted bit(1) DEFAULT 0,
+account_status BIT(1) default 0,
+is_deleted BIT(1) DEFAULT 0,
 PRIMARY KEY (id)
 );
 
@@ -21,7 +23,13 @@ email VARCHAR(255) NOT NULL,
 balance INT(255) DEFAULT 0,
 location VARCHAR(255),
 lt_online VARCHAR(255),
-account_status bit(1),
-is_deleted bit(1),
+account_status BIT(1),
+is_deleted BIT(1),
 PRIMARY KEY (id)
 );
+
+/* to prevent Brute Force */
+CREATE TABLE login_attempts (
+user_id INT(11) NOT NULL,
+time VARCHAR(30) NOT NULL
+)
